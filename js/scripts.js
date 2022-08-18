93920829b343c49ddeae8eecfe5135be00a9a114
Firadeth <firadeth@yandex.ru>
2022-08-18 17:52:41 +0300

    remove jquery(add hamburger js)

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,18 @@
-$(document).ready(function(){
-    $('.hamburger').click(function(){
-        $(this).toggleClass("active");
-        $(".navbar-menu").toggleClass("active");
+"use strict";
+
+const ham = document.querySelector('.hamburger'),
+      bar = document.querySelector('.navbar-menu'),
+      links = bar.querySelectorAll('a');
+      
+
+ham.addEventListener('click', () => {
+    ham.classList.toggle("active");
+    bar.classList.toggle("active");
+});
+
+links.forEach((link) => {
+    link.addEventListener('click', () => {
+        ham.classList.remove("active");
+        bar.classList.remove("active");
     });
 });
