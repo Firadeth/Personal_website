78cf5b3fa0c1a4289eb597709191b36be0fbb168
Firadeth <firadeth@yandex.ru>
2022-08-25 15:46:04 +0300

    Избавился от якорей

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,32 @@
-"use strict";
+window.addEventListener('DOMContentLoaded', () => {
 
-const ham = document.querySelector('.hamburger'),
-      bar = document.querySelector('.navbar-menu'),
-      links = bar.querySelectorAll('a');
-      
+    const ham = document.querySelector('.hamburger'),
+          bar = document.querySelector('.navbar-menu'),
+          links = bar.querySelectorAll('.navbar-item'),
+          section = document.querySelectorAll('section');
+        
 
-ham.addEventListener('click', () => {
-    ham.classList.toggle("active");
-    bar.classList.toggle("active");
-});
+    ham.addEventListener('click', () => {
+        ham.classList.toggle("active");
+        bar.classList.toggle("active");
+    });
 
-links.forEach((link) => {
-    link.addEventListener('click', () => {
-        ham.classList.remove("active");
-        bar.classList.remove("active");
+    links.forEach((link) => {
+        link.addEventListener('click', () => {
+            ham.classList.remove("active");
+            bar.classList.remove("active");
+        });
+    });
+
+    bar.addEventListener('click', (event) => {
+        const target = event.target;
+
+        if (target && target.classList.contains('navbar-item')) {
+            links.forEach((item, i) => {
+                if (event.target == item) {
+                    section[i].scrollIntoView();
+                }
+            });
+        }
     });
 });
